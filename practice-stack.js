56ceb528534221f0638f641b0e89b5b137c9b32a
CaityHeath <caity.heath@gmail.com>
2019-02-13 08:14:18 -0800

    ll stack and queues

---
+++ practice-stack.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Stack{
+  constructor(){
+    this.top = null;
+    this.next = null;
+    this.length = 0;
+  }
+
+  push(value){
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+    this.length++;
+  }
+
+  pop(){
+    let temp = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  peek(){
+    return this.top.value;
+  }
+}
+
+
+function print(stack){
+  let temp;
+  while(stack.top){
+    temp = stack.pop();
+    console.log(temp);
+  }
+  return;
+}
